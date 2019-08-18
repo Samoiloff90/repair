bb7747cc0969ccf1b5cc0fc3cd5c493ecc8b6dd0
unknown <e.e.samoilov@mail.ru>
2019-08-18 12:28:07 +0400

    Исправил ошибки валидатора

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,7 @@
 var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
-
-gulp.task('default', defaultTask);
-
-function defaultTask(done) {
-  done();
-}
+var htmlmin = require('gulp-htmlmin');
+var tinyPNG = require('gulp-tinypng-compress');
 
 gulp.task('minify-css', function(done) {
   return gulp.src('./src/css/*.css')
@@ -17,13 +13,46 @@ gulp.task('minify-css', function(done) {
     done();
 });
 
-gulp.task('minify-js', function (done) {
-  return gulp.src('./src/css/*.css')
-    .pipe(cleanCSS({
-      compatibility: 'ie8'
-    }))
-    .pipe(gulp.dest('dist/css/'))
+gulp.task('move-js', function (done) {
+  return gulp.src('./src/js/*.js')
+  .pipe(gulp.dest('dist/js/'))
+  done();
+  
+});
 
+gulp.task('htmlmin', function (done) {
+  return gulp.src('./src/*.html')
+    .pipe(gulp.dest('dist/js/'))
+  done();
+
+});
+
+var htmlmin = require('gulp-htmlmin');
+gulp.task('htmlmin', function (done) {
+  return gulp.src('./src/*.html')
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(gulp.dest('dist/'))
   done();
 });
 
+gulp.task('fonts', function (done) {
+  return gulp.src('./src/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'))
+  done();
+});
+
+gulp.task('tinypng', function (done) {
+  gulp.src('./src/img/**/*.{png,jpg,jpeg}')
+    .pipe(tinyPNG({
+      key: 'qTG1s8ByLbVMJhCPJdns3hHfhg2R3N3c',
+    }))
+    .pipe(gulp.dest('dist/img/'));
+    done();
+});
+
+gulp.task('default', gulp.parallel('minify-css', 'move-js', 'fonts', 'htmlmini', 'tinypng', function (done) {
+  // do more stuff
+  done();
+}));
